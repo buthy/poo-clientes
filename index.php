1ab5ffb9235def7adcad9f95ea1e6fcc38ccbd15
Marcus David <buthy88@gmail.com>
2015-10-01 19:33:18 +0000

    Correções nos caminhos

--- index.php
+++ index.php
@@ -3,9 +3,6 @@
 ini_set('error_reporting', E_ALL);
 ini_set('default_charset', 'UTF-8');
 
-$url = 'http://' . $_SERVER['HTTP_HOST'] . '/www/cursos/poo-clientes';
-
-define('BASE_URL', $url);
 define('PUBLIC_HTML', __DIR__ . DIRECTORY_SEPARATOR);
 define('SRC_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR);
 define('VENDOR_NAME', 'app');
